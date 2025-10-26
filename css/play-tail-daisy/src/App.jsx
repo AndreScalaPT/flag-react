b0195ae6ccc7e2c2f3181b66a2a16d08d59e6d06
André Scala <andrescalamail@gmail.com>
2025-10-26 09:43:14 +0100

    atualização do exercicio

---
+++ css/play-tail-daisy/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import WelcomeModal from "./components/WelcomeModal";
+
+function App() {
+  return (
+    <>
+      <WelcomeModal />
+    </>
+  );
+}
+
+export default App;
---
+++ css/play-tail-daisy/src/components/WelcomeModal.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+
+export default function WelcomeModal() {
+/* - função que permite executar código automaticamente quando o componente é mostrado no ecrã
+   - assim que o componente WelcomeModal aparece, o código dentro do useEffect é executado.
+ */  useEffect(() => {
+    const modal = document.getElementById("my_modal_1");
+    if (modal) {
+      modal.showModal();
+    }
+  }, []);
+  
+  return (
+    <>
+{/* <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_1").showModal()}
+      >
+        open modal
+      </button> */}
+
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <div className="flex justify-center"></div>
+          <picture className="flex justify-center">
+            <source
+              srcSet="https://fonts.gstatic.com/s/e/notoemoji/latest/1f47b/512.webp"
+              type="image/webp"
+            />
+            <img
+              src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f47b/512.gif"
+              alt="👻"
+              width="64"
+              height="64"
+            />
+          </picture>
+
+          <p className="py-4 flex justify-center">
+            Chegas-te a um sítio muito perigoso, tens a certeza que queres
+            continuar? Clica em Continuar ou ESC, os dois levam-te por caminhos
+            longínquos... !
+          </p>
+
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Continuar</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
+}
