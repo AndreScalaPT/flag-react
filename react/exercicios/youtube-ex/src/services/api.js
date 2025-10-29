f8de1f470742c2534bb271301ad3b7a957ce4d03
André Scala <andrescalamail@gmail.com>
2025-10-29 18:36:12 +0100

    Exercício com FETCH e mais outras funções avançadas

---
+++ react/exercicios/youtube-ex/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "b2abf08cd9d423185679ec6109d1a822";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
